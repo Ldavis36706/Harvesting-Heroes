8ca799f14cc53c2e7cfdaa7d19448b0f762cebc3
Lakesha Hunter <159222943+Ldavis36706@users.noreply.github.com>
2025-05-15 21:39:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { UserManager } from "https://cdn.skypack.dev/oidc-client-ts";
 const cognitoAuthConfig = {
     authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_yC2rfcpje",
     client_id: "2l39pn13rfbasb0ht89i2fhpvq",
-    redirect_uri: "https://d84l1y8p4kdic.cloudfront.net/index.html",  // your site domain
+    redirect_uri: "https://harvestingheroes.com/",  // your site domain
     response_type: "code",
     scope: "email openid phone"
 };
@@ -14,7 +14,7 @@ export const userManager = new UserManager({
 
 export async function signOutRedirect () {
     const clientId = "2l39pn13rfbasb0ht89i2fhpvq";
-    const logoutUri = "https://d84l1y8p4kdic.cloudfront.net";
+    const logoutUri = "https://harvestingheroes.com/";
     const cognitoDomain = "https://us-east-1yc2rfcpje.auth.us-east-1.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 }
